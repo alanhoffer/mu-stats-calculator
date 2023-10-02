5b1654aeb9fca84a4236cded895a6046a996082d
Alan Hoffer <alanhoffer97@hotmail.com>
2023-10-01 21:37:26 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { STATS_CONSTANT, RESET_POINTS, LEVEL_POINTS } from './constants';
 
 function App() {
+
+  const [stats, setStats] = useState({ str: 0, agi: 0, vit: 0, ene: 0, total: 0 });
+  const [clase, setClase] = useState('bk')
+  const [resets, setResets] = useState(0);
+  const [level, setLevel] = useState(0);
+
+  useEffect(() => {
+    let final_stats = { str: 0, agi: 0, vit: 0, ene: 0, total: 0 };
+    let total_points = level * LEVEL_POINTS + resets * RESET_POINTS;
+
+    final_stats.str = (total_points * STATS_CONSTANT[clase].str) / 50000;
+    final_stats.agi = (total_points * STATS_CONSTANT[clase].agi) / 50000;
+    final_stats.vit = (total_points * STATS_CONSTANT[clase].vit) / 50000;
+    final_stats.ene = (total_points * STATS_CONSTANT[clase].ene) / 50000;
+    final_stats.total = (total_points * STATS_CONSTANT[clase].total) / 50000;
+
+    setStats(final_stats)
+
+  }, [clase, level, resets])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='stats-container'>
+
+        <p> STR: {stats.str} </p>
+        <p> AGI: {stats.agi} </p>
+        <p> VIT: {stats.vit} </p>
+        <p> ENE: {stats.ene} </p>
+        <p> TUS PUNTOS: {stats.total} </p>
+
+        <div className='stats-form'>
+          <p>Level</p>
+          <input
+            id="name"
+            type="text"
+            value={level}
+            onChange={(e) => setLevel(e.target.value)}
+          />
+          <p>Reset</p>
+          <input
+            id="name"
+            type="text"
+            value={resets}
+            onChange={(e) => setResets(e.target.value)}
+          />
+          <p>Clase</p>
+          <select
+            value={clase}
+            onChange={e => setClase(e.target.value)}
+          >
+            <option value="bk">BLADE KNIGHT</option>
+            <option value="elf_agi">ELF_AGI</option>
+            <option value="elf_ene">ELF_ENE</option>
+            <option value="sum">SUM</option>
+            <option value="mg_caballero">MG_CABALLERO</option>
+            <option value="mg_mago">MG_MAGO</option>
+            <option value="mg_balanced">MG_BALANCED</option>
+            <option value="sm">WIZARD</option>
+          </select>
+        </div>
+      </div>
+
     </div>
   );
 }
---
+++ src/constants.js
@@ -0,0 +1,16 @@
+let STATS_CONSTANT = {
+  bk: { str: 20000, agi: 15000, vit: 5000, ene: 10000, total: 50000 },
+  elf_agi: { str: 12000, agi: 32000, vit: 5000, ene: 1000, total: 50000 },
+  elf_ene: { str: 20000, agi: 15000, vit: 5000, ene: 10000, total: 50000 },
+  sum: { str: 5000, agi: 15000, vit: 5000, ene: 25000, total: 50000 },
+  dl: { str: 10000, agi: 15000, vit: 5000, ene: 20000, total: 50000 },
+  mg_caballero: { str: 23000, agi: 20000, vit: 5000, ene: 2000, total: 50000 },
+  mg_mago: { str: 5000, agi: 15000, vit: 5000, ene: 25000, total: 50000 },
+  mg_balanced: { str: 15000, agi: 15000, vit: 5000, ene: 15000, total: 50000 },
+  sm: { str: 1000, agi: 20000, vit: 4000, ene: 25000, total: 50000 },
+};
+
+let RESET_POINTS = 820;
+let LEVEL_POINTS = 5;
+
+export {STATS_CONSTANT, RESET_POINTS, LEVEL_POINTS }
